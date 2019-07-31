e8f240697c4a1f73a17af5e3ed7e103c6b149931
Clinton James <clintjeez@gmail.com>
2019-07-31 14:51:53 +0100

    initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,13 @@
+
+// typed animation
+
+new Typed('#typed',{
+    strings : ['Connect .Learn .Create','Kick-start a new Career', 'Turning ideas into reality'],
+    typeSpeed : 200,
+    delaySpeed : 200,
+    loop : true
+  });
+
+
+  // WOW JS ANIMATION ACTIVATE 
+  new WOW().init();
